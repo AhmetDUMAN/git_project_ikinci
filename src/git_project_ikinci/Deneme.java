062b797074e3579eafe9c3b038ec1de793a70cb7
Ahmet <ahmetpower2651@gmail.com>
2020-08-10 01:17:00 +0300

    conflict ornegi

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -3,9 +3,10 @@ package git_project_ikinci;
 public class Deneme {
 
 	public static void main(String[] args) {
-		System.out.println("Ýkinci git projem.");
-		System.out.println("ikinci satýr kodum");
-		System.out.println("remote değişiklik");
+		System.out.println("Ã�kinci git projem.");
+		System.out.println("ikinci satÃ½r kodum");
+		System.out.println("remote deÄŸiÅŸiklik");
+		System.out.println("selamlar - hamza");
 	}
 
 }
